0e7d12dd9572e3da87fbb852bc8ba658b98903e1
= <=>
2023-10-20 23:32:07 +0530

    index common file for import export

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,12 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
-
+import Logo from "./Logo";
+import Container from "./container/Container";
+import LogoutBtn from './Header/LogoutBtn'
 export
- {      Header,
-        Footer
+{      Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
     } 
