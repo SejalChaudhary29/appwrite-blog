8e42639a82ee2f6a71825739f80b77ebf185def2
= <=>
2023-11-04 11:00:09 +0530

    app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import {login , logout} from './store/authSlice'
 import './App.css'
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
- 
+ import {Outlet} from 'react-router-dom'
 function App() {
 const [loading , setLoading ] = useState(true)
 const dispatch = useDispatch()
@@ -24,7 +24,7 @@ useEffect(()=>{
   <div className="w-full block">
     <Header/>
     <main>
-      {/* <Outlet/>  */}
+    {/* <Outlet/> */}
     </main>
     <Footer/>
   </div>
